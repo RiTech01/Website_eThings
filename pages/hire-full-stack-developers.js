7f0e7f147ecf18a1602da61effb5bb4387fd0d17
RiTech01 <rishabh.jain@ething.in>
2026-09-14 18:38:57 +0530

    Add full stack developers landing route

---
+++ pages/hire-full-stack-developers.js
@@ -0,0 +1,4 @@
+import PaidSearchLandingPage from '../components/PaidSearchLandingPage';
+import { pages } from '../data/landingPages';
+export default function HireFullStackDevelopers(){return <PaidSearchLandingPage page={pages.fullStack}/>;}
+
