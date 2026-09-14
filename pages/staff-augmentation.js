28da3d8762304985cd3478db01cf528e37089a8b
RiTech01 <rishabh.jain@ething.in>
2026-09-14 18:39:14 +0530

    Add staff augmentation landing route

---
+++ pages/staff-augmentation.js
@@ -0,0 +1,4 @@
+import PaidSearchLandingPage from '../components/PaidSearchLandingPage';
+import { pages } from '../data/landingPages';
+export default function StaffAugmentation(){return <PaidSearchLandingPage page={pages.staff}/>;}
+
