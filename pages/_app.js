f6e6df6f21269706d9a81ae1f4250236e637b9f3
RiTech01 <rishabh.jain@ething.in>
2026-09-14 22:09:23 +0530

    Load Why Ething visual treatment

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.css';
 import '../styles/landing-enhancements.css';
+import '../styles/why-ething.css';
 
 export default function App({ Component, pageProps }) { return <Component {...pageProps} />; }
 
