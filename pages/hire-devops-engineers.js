ecd199fed771249a3f2edf3e32294c8761a1e883
RiTech01 <rishabh.jain@ething.in>
2026-09-14 18:39:09 +0530

    Add DevOps engineers landing route

---
+++ pages/hire-devops-engineers.js
@@ -0,0 +1,4 @@
+import PaidSearchLandingPage from '../components/PaidSearchLandingPage';
+import { pages } from '../data/landingPages';
+export default function HireDevopsEngineers(){return <PaidSearchLandingPage page={pages.devops}/>;}
+
