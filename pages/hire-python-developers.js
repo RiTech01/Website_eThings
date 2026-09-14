396ceb294a607b2299a526791a7ad640fb56c92b
RiTech01 <rishabh.jain@ething.in>
2026-09-14 18:38:52 +0530

    Add Python developers landing route

---
+++ pages/hire-python-developers.js
@@ -0,0 +1,4 @@
+import PaidSearchLandingPage from '../components/PaidSearchLandingPage';
+import { pages } from '../data/landingPages';
+export default function HirePythonDevelopers(){return <PaidSearchLandingPage page={pages.python}/>;}
+
