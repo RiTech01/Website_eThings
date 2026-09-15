f1c00827a78c17877e6bf84b3eb9b8ea29f32657
RiTech01 <rishabh.jain@ething.in>
2026-09-15 18:42:12 +0530

    Load creative campaign page styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import '../styles/landing-enhancements.css';
 import '../styles/why-ething.css';
+import '../styles/creative-campaign.css';
 
 export default function App({ Component, pageProps }) { return <Component {...pageProps} />; }
 
